aef5b7dbe406a7080874c65f45ea423dc856c628
FerrarioTommaso <t.ferrario@campus.unimib.it>
2022-06-09 09:20:22 +0200

    Creazione progetto spring

---
+++ SistemaBancarioRest/src/main/java/com/SistemaBancarioRest/SistemaBancarioRestApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaBancarioRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBancarioRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBancarioRestApplication.class, args);
+	}
+
+}
